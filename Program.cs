d3fc1683f1014b99fb7b8cbd8ed15b45793efebc
lilbitvoid <91071069+lilbitvoid@users.noreply.github.com>
2023-02-27 19:24:37 +0300

    Add DBRepository

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BookDb>(options => {
     options.UseNpgsql(builder.Configuration.GetConnectionString("NpgSql"));
 });
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -11,49 +12,29 @@ if (app.Environment.IsDevelopment())
     db.Database.EnsureCreated();
 }
 
-app.MapGet("/books", async (BookDb db) => await db.Books.ToListAsync());
-app.MapGet("/books/{id}", async (int id, BookDb db) => 
-    await db.Books.FirstOrDefaultAsync(b => b.Id == id) is Book book
+app.MapGet("/books", async (IBookRepository bookRepository) => Results.Ok(await bookRepository.GetBooksAsync()));
+app.MapGet("/books/{id}", async (int id, IBookRepository bookRepository) => 
+    await bookRepository.GetBookAsync(id) is Book book
     ? Results.Ok(book)
     : Results.NotFound());
 
-app.MapPost("/books", async ([FromBody]Book book, BookDb db) => {
-    await db.Books.AddAsync(book);
-    await db.SaveChangesAsync();
+app.MapPost("/books", async ([FromBody]Book book, IBookRepository bookRepository) => {
+    await bookRepository.InsertBookAsync(book);
+    await bookRepository.SaveAsync();
     return Results.Created($"/books/{book.Id}", book);
 });
-app.MapPut("/books", async (Book book, BookDb db) => 
+app.MapPut("/books", async ([FromBody]Book book, IBookRepository bookRepository) => 
 {
-    var bookFromDb = await db.Books.FindAsync(book.Id);
-    if (bookFromDb is null) throw new Exception("Index not found");
-    bookFromDb.Name = book.Name;
-    bookFromDb.Author = book.Author;
-    bookFromDb.YearPubl = book.YearPubl;
-    await db.SaveChangesAsync();
+    await bookRepository.UpdateBookAsync(book);
+    await bookRepository.SaveAsync();
     return Results.NoContent();
 });
 
-app.MapDelete("/books/{id}", async (int id, BookDb db) => 
+app.MapDelete("/books/{id}", async (int id, IBookRepository bookRepository) => 
 {
-    var bookFromDb = await db.Books.FindAsync(id);
-    if (bookFromDb is null) return Results.NotFound();
-    db.Books.Remove(bookFromDb);
-    await db.SaveChangesAsync();
+    await bookRepository.DeleteBookAsync(id);
+    await bookRepository.SaveAsync();
     return Results.NoContent();
 });
 app.UseHttpsRedirection();
 app.Run();
-
-public class BookDb: DbContext 
-{
-    public BookDb(DbContextOptions options) : base (options) {}
-    public DbSet<Book> Books => Set<Book>();
-}
-
-public class Book 
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Author { get; set; } = string.Empty;
-    public string YearPubl { get; set; } = string.Empty;
-}
